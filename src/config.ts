5124baaa12925fb00a43a5158f55d98fc820e55d
Alexander Ciesielski <alexander@ciesielski.co>
2020-03-26 14:52:38 +0100

    add detailed instructions on how to run this as an addon in HA

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 import { IHAConfig } from 'tsnode-homeassistant';
+import { HA_HOST, HA_PORT, HA_TOKEN } from './config-secret';
 
 export const CONFIG: IHAConfig = {
-  host: 'myHaHost',
-  token: 'myHaToken'
+  // Create a config-secret.ts folder in the same directory
+  // and export the following three constants
+  host: HA_HOST,
+  port: HA_PORT,
+  token: HA_TOKEN,
 };
